87fea7cacb9d209f8332c1fdc3ae376820192e4f
Jiawei <liujiaweidreamer@gmail.com>
2021-11-24 21:38:58 +0800

    myBind 是一个函数

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,12 @@ const myBind = require('../src/index.js');
 
 Function.prototype.myBind = myBind;
 
-const fn = ()=>{}
+test1('✅ 1.️ myBind 是一个函数');
 
-console.log(fn.myBind);
+function test1(msg) {
+  const fn = () => {};
+
+  console.assert(typeof fn.myBind === 'function');
+
+  console.log(msg);
+};
